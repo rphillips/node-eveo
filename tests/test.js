49cde72b8a13a9ea9133cc7f03a98388aee690cf
Ryan Phillips <ryan@trolocsis.com>
2011-07-17 16:49:01 -0500

    add authentication support

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,13 @@
 var eveo = require('../lib/main');
 var util = require('util');
+var fs = require('fs');
+var path = require('path');
 
-var cli = new eveo.Client('blah', 'blah');
-cli.skillTree(function(err, data) {
+var authFilePath = path.join(process.env.HOME, '.node-eveo');
+var authfile = fs.readFileSync(authFilePath);
+var auth = JSON.parse(authfile);
+
+var cli = new eveo.Client(auth.ID, auth.Key);
+cli.characters(function(err, data) {
   console.log(util.inspect(data, true, null));
 });
